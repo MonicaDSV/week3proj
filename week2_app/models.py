6fc23bf1c9789566d84f1f8dbcc58afc06926b2e
Robert Owens <raowens15@icloud.com>
2020-02-19 17:20:58 -0500

    models now interact with the index.html

--- week2_app/models.py
+++ week2_app/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Course(models.Model):
+    course_name = models.CharField(max_length=50)
 
+    def __str__ (self):
+        return "%s" % (self.course_name)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Task(models.Model):
+    discription = models.CharField(max_length=200)
+    time_due = models.DateTimeField
+
+    def __str__ (self):
+        return "%s due: %s" % (self.discription, self.time_due)
--- week2_app/views.py
+++ week2_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Course, Task
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    courses = Course.objects.order_by('course_name')
+    context = {"courses":courses}
+    return render(request, 'index.html', context)
